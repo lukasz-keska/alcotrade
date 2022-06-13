da7bf6c1ff9d460665b2c1b438d4650031a6501a
lukasz.keska <lukasz.keska@gmail.com>
2022-06-13 22:28:31 +0200

    chore: first commit

---
+++ protected/views/layouts/cookies.php
@@ -0,0 +1,13 @@
+<div style="display: block; z-index: 10000; position: fixed; background-color: rgb(255, 255, 255); font-size: 12px; color: rgb(0, 0, 0); bottom: 0px; left: 0px; width: 100%; box-shadow: rgb(0, 0, 0) 0px -2px 2px;" class="CookieMonsterBox">
+    <div style="margin:10px" class="">
+        Ta strona wykorzystuje pliki cookies, korzystanie z niej oznacza zgodę na ich zapis w Twoim urządzeniu. 
+        Jeśli nie zgadzasz się na to zmień ustawienia cookies w przeglądarce.<button style="margin-left:10px" class="CookieMonsterOk" name="yt0" type="button">Rozumiem</button>
+    </div>
+</div>
+
+<script type="text/javascript" src="/js/CookieMonster.js"></script>
+<script type="text/javascript">
+/*<![CDATA[*/
+CookieMonster.init({"path":"\/","expires":30,"secure":false,"classOuter":"CookieMonsterBox","classInner":"","classButton":"CookieMonsterOk"});
+/*]]>*/
+</script>
